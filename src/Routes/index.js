a6cecd31065f9a41b6a7c9c0121b4d52c6a59ee3
José Cortés <cote99salamanca@gmail.com>
2020-10-17 23:48:58 -0300

    se configura .gitignore, se elimina babel del proyecto y se modifican las rutas

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const apiEnterpriseRouter = require('./Api/Enterprise');
+
 router.use('/enterprises', apiEnterpriseRouter);
 
 module.exports = router;
