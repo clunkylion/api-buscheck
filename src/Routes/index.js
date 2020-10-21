eeb9c9818f41d2ca73f3dd921a259d33f4850e15
José Cortés <cote99salamanca@gmail.com>
2020-10-21 00:37:23 -0300

    se agrega city al las rutas

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiEnterpriseRouter = require('./../Enterprise');
-
+const apiCityRouter = require('./../City');
 router.use('/enterprises', apiEnterpriseRouter);
-
+router.use('/cities', apiCityRouter);
 module.exports = router;
