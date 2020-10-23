1642b082b621ecad7716e6a4890550854307bb80
José Cortés <cote99salamanca@gmail.com>
2020-10-23 13:03:18 -0300

    se cambia el la columna phone de string a int

--- src/Entities/Enterprise/Enterprise.model.js
+++ src/Entities/Enterprise/Enterprise.model.js
@@ -1,22 +1,25 @@
 const { DataTypes, Model } = require('sequelize');
 const connection = require('../../Lib/Mysql');
 
-class Enterprise extends Model {};
+class Enterprise extends Model {}
 
-Enterprise.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+Enterprise.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    phone: DataTypes.INTEGER,
+    email: DataTypes.STRING,
   },
-  name: DataTypes.STRING,
-  address: DataTypes.STRING,
-  city: DataTypes.STRING,
-  phone: DataTypes.STRING,
-  email: DataTypes.STRING,
-},{
-  sequelize: connection,
-  modelName: 'Enterprise'
-})
+  {
+    sequelize: connection,
+    modelName: 'Enterprise',
+  }
+);
 
 module.exports = Enterprise;
