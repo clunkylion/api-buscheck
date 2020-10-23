0522c93a18c0ef26b7371d9e6bd718a0e710c4cc
José Cortés <cote99salamanca@gmail.com>
2020-10-23 00:03:48 -0300

    se agrega la ruta para role y People

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const apiEnterpriseRouter = require('../Entities/Enterprise');
 const apiCityRouter = require('../Entities/City');
+const apiRoleRouter = require('../Entities/Role');
+const apiPeopleRouter = require('../Entities/People');
 router.use('/enterprises', apiEnterpriseRouter);
 router.use('/cities', apiCityRouter);
+router.use('/roles', apiRoleRouter);
+router.use('/peoples', apiPeopleRouter);
 module.exports = router;
