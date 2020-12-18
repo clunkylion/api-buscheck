fa9b5b38d93bed4394b09a079564149b10a13d04
José Cortés <cote99salamanca@gmail.com>
2020-12-18 02:07:19 -0300

    Se modifican el metodo getALl y getById para que traiga los datos asociados de la tabla

--- src/Entities/Origin/Origin.controller.js
+++ src/Entities/Origin/Origin.controller.js
@@ -1,10 +1,10 @@
 const Origin = require('./Origin.model');
-
+const Station = require('../Station/Station.model');
 const controller = {};
 
 controller.getAll = async (req, res) => {
   try {
-    const origins = await Origin.findAll();
+    const origins = await Origin.findAll({ include: Station });
     res.status(200).json({ origins });
   } catch (err) {
     res.status(500);
@@ -15,6 +15,7 @@ controller.getById = async (req, res) => {
   try {
     const origin = await Origin.findOne({
       where: { id: req.params.originId },
+      include: { model: Station },
     });
     console.log(origin);
     res.status(200).json({ origin });
@@ -27,7 +28,7 @@ controller.create = async (req, res) => {
   try {
     const body = req.body;
     const origin = await Origin.create({
-      StationId: body.stationId,
+      StationId: body.StationId,
     });
     res.status(201).json({ origin });
   } catch (err) {
